bb4be3e571eb0f74683ccc13f90cfa5b2c0658da
KhairunNisaDaryD <71258340+KhairunNisaDaryD@users.noreply.github.com>
2020-09-28 23:04:18 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,1 +1,30 @@
+<?php
+
+if ($_GET) :
+    $bilangan1 = $_GET['bilangan1'];
+    $bilangan2 = $_GET['bilangan2'];
+    $operasi = $_GET['operasi'];
+
+    switch ($operasi) {
+        case '+':
+            $hasil = $bilangan1 + $bilangan2;
+            break;
+        case '-':
+            $hasil = $bilangan1 - $bilangan2;
+            break;
+        case '*':
+            $hasil = $bilangan1 * $bilangan2;
+            break;
+        case '/':
+            $hasil = $bilangan1 / $bilangan2;
+            break;
+    }
+?>
+    <br>
+    Bilangan 1 : <strong><?php echo $bilangan1 ?></strong><br>
+    Bilangan 2 : <strong><?php echo $bilangan2 ?></strong><br>
+    Hasil      : <strong> <?php echo $hasil ?></strong><br>
+
+<?php
+endif; ?>
 
